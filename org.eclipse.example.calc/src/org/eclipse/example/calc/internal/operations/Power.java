af8faa5ba94486539ab8d1aae42452502bc36c30
Cody Phillips <CPRSprint1@users.noreply.github.com>
2026-09-13 20:41:21 -0400

    Add Power operation to calculator

---
+++ org.eclipse.example.calc/src/org/eclipse/example/calc/internal/operations/Power.java
@@ -0,0 +1,21 @@
+package org.eclipse.example.calc.internal.operations;
+
+import org.eclipse.example.calc.BinaryOperation;
+
+/**
+ * Binary Power Operation
+ */
+
+public class Power extends AbstractOperation implements BinaryOperation {
+	
+	@Override
+	public float perform(float arg1, float arg2) {
+		return (float) Math.pow(arg1, arg2);
+	}
+	
+	@Override
+	public String getName() {
+		return "^";
+	}
+
+}
