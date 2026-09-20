0e57e4c2137d0f270b57468923b0a3a80f6091dd
Cody Phillips <CPRSprint1@users.noreply.github.com>
2026-09-20 19:05:12 -0400

    Handles division by zero

--- org.eclipse.example.calc/src/org/eclipse/example/calc/internal/operations/Divide.java
+++ org.eclipse.example.calc/src/org/eclipse/example/calc/internal/operations/Divide.java
@@ -10,6 +10,10 @@ public class Divide extends AbstractOperation implements BinaryOperation {
 	
 	@Override
 	public float perform(float arg1, float arg2) {
+		if (arg2 == 0) {
+			throw new ArithmeticException("Cannot divide by zero");
+		}
+		
 		return arg1 / arg2;
 	}
 	
